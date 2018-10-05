79ab7bdd8ed1f3c56f3538de2805789831918ea7
Maverick Liberty <maverick.liberty29@gmail.com>
2018-10-04 22:03:29 -0400

    tests/heads.py: Unit test file for making the classic Arcade Mode arena.
    Former-commit-id: 22f8b1bd3e572f09891981f437dc51a7cd6169b9

--- game/tests/heads.py
+++ game/tests/heads.py
@@ -1,3 +1,4 @@
+"""
 from lib.coginvasion.standalone.StandaloneToon import *
 
 from lib.coginvasion.quest.Quest import Quest
@@ -10,6 +11,7 @@ from lib.coginvasion.quest.QuestPoster import QuestPoster
 from lib.coginvasion.quest import QuestGlobals
 from lib.coginvasion.quest import RewardType
 
+
 quest = Quest("The Hell Begins", 0, "3", [QuestReward(RewardType.LAFF_POINTS, 2)])#[QuestReward(RewardType.LAFF_POINTS, 3), QuestReward(RewardType.LAFF_POINTS, 2)])
 quest.addObjective(CogObjective(quest, 2000, [], 10, name = 'Big Wig'))
 #quest.addObjective(RecoverItemObjective(20, 'Powder', quest, [], 1000, name = 'Cold Caller'))
@@ -18,5 +20,48 @@ quest.addObjective(CogObjective(quest, 2000, [], 10, name = 'Big Wig'))
 poster = QuestPoster(quest)
 poster.reparentTo(aspect2d)
 poster.update()
+"""
 
-run()
+import test_base
+import src.coginvasion.standalone.Standalone
+
+model = loader.loadModel('phase_5.5/models/estate/terrain.bam')
+model.reparentTo(render)
+
+from panda3d.core import TransparencyAttrib
+from src.coginvasion.shop.DistributedBattleShop import DistributedBattleShop
+from src.coginvasion.shop.DistributedGagShop import DistributedGagShop
+
+#bShop = DistributedBattleShop(base.cr)
+#bShop.generate()
+#bShop.setupClerk()
+gShop = DistributedGagShop(base.cr)
+base.cr.createDistributedObject(distObj = gShop)
+gShop.setParent(2)
+gShop.reparentTo(render)
+#gShop.clerk.reparentTo(gShop)
+print gShop.getParent()
+#gShop.reparentTo(render)
+
+def processChildren(node):
+	if len(node.getChildren()) > 0:
+		for child in node.getChildren():
+			print 'Applying transparency to node: {0}.'.format(child.getName())
+			print child.getTexture()
+			child.setTransparency(TransparencyAttrib.M_multisample, 1)
+			child.setDepthOffset(1, 1)
+			#child.setBin('shadow', 19)
+			child.clearModelNodes()
+			child.flattenStrong()
+			print 'Applied bin'
+			processChildren(child)
+
+#dBase.setBin('ground', 18)
+#processChildren(dBase)
+#print 'Applied fix'
+
+gShop.clerk.reparentTo(gShop)
+base.oobe()
+
+base.startDirect()
+base.run()
