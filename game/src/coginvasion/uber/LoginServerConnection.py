9d7d2d93d729acf49dfa00452ea2fb53ee06d998
Maverick Liberty <maverick.liberty29@gmail.com>
2019-03-24 02:00:47 -0400

    LoginServerConnection: Updated uber -> login server connection code to fix issues. It seems to be working correctly now.
    Former-commit-id: 67664559c2ddd891a2d958d05b3ca62bfd19a6ea

--- game/src/coginvasion/uber/LoginServerConnection.py
+++ game/src/coginvasion/uber/LoginServerConnection.py
@@ -9,73 +9,81 @@ Copyright (c) CIO Team. All rights reserved.
 """
 
 from direct.directnotify.DirectNotifyGlobal import directNotify
-from threading import Thread
 from LoginToken import LoginToken
-import SocketServer
+import socket
+import thread
+import threading
 
 uberRepo = None
 
 SRV_CREATE_TOKEN = 0
 SRV_NTWK_MESSAGE = 1
 
-class LoginServerConnectionHandler(SocketServer.BaseRequestHandler):
-    notify = directNotify.newCategory('LoginServerConnectionHandler')
-    
-    def handle(self):
-        self.data = []
-        try:
-            for s in self.request.recv(2048).splitlines():
-                s = s.strip()
-                
-                if len(self.data) == 0:
-                    # The very first line is going to be the id of the datagram.
-                    # Some weird ASCII characters begin the lines, so let's just use the
-                    # last character of the first line because our ids range from [0-9].
-                    s = s[-1]
-                
-                self.data.append(s)
-            
-            if len(self.data) > 0:
-                datagramId = int(self.data[0])
-                
-                if datagramId == SRV_CREATE_TOKEN:
-                    ipAddress = self.data[1]
-                    token = self.data[2]
-                    self.generateToken(ipAddress, token)
-                elif datagramId == SRV_NTWK_MESSAGE:
-                    message = self.data[1]
-                    self.sendNetworkMessage(message)
-        except Exception:
-            self.notify.warning('Client disconnected while processing login token data.')
-            return
-    
-    def generateToken(self, ipAddress, token):
-        global uberRepo
-        uberRepo.storeToken(LoginToken(token, ipAddress))
-        self.notify.debug('Stored token %s for IP: %s' % (token, ipAddress))
-    
-    def sendNetworkMessage(self, message):
-        global uberRepo
-        uberRepo.csm.d_networkMessage(message)
-        self.notify.debug('Sent network message!')
-
 class LoginServerConnection:
     notify = directNotify.newCategory('LoginServerConnection')
     
     def __init__(self, repository, port):
         self.server = None
-        Thread(target = self.startServer, args = (port,)).start()
+        
+        self.socket = socket.socket()
+        self.socket.bind(('localhost', port))
+        self.socket.listen(5)
+        self.closeRequested = False
+        
+        thread.start_new_thread(self.handleConnection, ())
         self.notify.info('Successfully started LoginServerConnection Server!')
         
         global uberRepo
         uberRepo = repository
         
-    def startServer(self, port):
-        self.server = SocketServer.TCPServer(('localhost', port), LoginServerConnectionHandler)
-        self.server.serve_forever()
+    def handleConnection(self):
+        while not self.closeRequested:
+            socket, ipAddress = self.socket.accept()
+            threading.Thread(target = self.handleClient, args = (socket, ipAddress)).start()
+        self.socket.close()
+        
+    def handleClient(self, socket, ipAddress):
+        self.notify.info('Opened a connection!')
+        while not self.closeRequested:
+            data = []
+            for s in socket.recv(2048).splitlines():
+                s = s.strip()
+                
+                if len(data) == 0:
+                    # The very first line is going to be the id of the datagram.
+                    # Some weird ASCII characters begin the lines, so let's just use the
+                    # last character of the first line because our ids range from [0-9].
+                    s = s[-1]
+                    
+                    try:
+                        int(s)
+                        data.append(s)
+                    except ValueError:
+                        # This means that what we got wasn't an int
+                        pass
+                else:
+                    data.append(s)
+            
+            if len(data) > 0:
+                datagramId = int(data[0])
+                
+                if datagramId == SRV_CREATE_TOKEN:
+                    ipAddress = data[1]
+                    token = data[2]
+                    self.generateToken(ipAddress, token)
+                elif datagramId == SRV_NTWK_MESSAGE:
+                    message = data[1]
+                    self.sendNetworkMessage(message)
+            
+    def generateToken(self, ipAddress, token):
+        global uberRepo
+        uberRepo.storeToken(LoginToken(token, ipAddress))
+        self.notify.info('Stored token %s for IP: %s' % (token, ipAddress))
     
+    def sendNetworkMessage(self, message):
+        global uberRepo
+        uberRepo.csm.d_networkMessage(message)
+        self.notify.info('Login Server published this network message: {0}.'.format(message))
+        
     def close(self):
-        if self.server:
-            self.server.shutdown()
-            self.server.server_close()
-            self.server = None
+        self.closeRequested = True
