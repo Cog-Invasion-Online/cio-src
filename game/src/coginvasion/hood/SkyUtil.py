735ad9ca921381ca48dcf0262a4060e8f1dc4b1f
Maverick Liberty <maverick.liberty29@gmail.com>
2019-04-05 21:51:47 -0400

    SkyUtil: Fixed skybox bug where clouds would clip through the environment occasionally.
    Former-commit-id: 703fe6488123d42efd741bc1a64acacb8be7ecff

--- game/src/coginvasion/hood/SkyUtil.py
+++ game/src/coginvasion/hood/SkyUtil.py
@@ -17,25 +17,34 @@ class SkyUtil:
     
     notify = directNotify.newCategory("SkyUtil")
     
+    def __init__(self):
+        self.prepared = False
+    
+    def prepareSky(self, sky):
+        if not self.prepared:
+            sky.setAttrib(BSPFaceAttrib.makeIgnorePvs(), 1)
+            sky.setLightOff(1)
+            sky.setFogOff(1)
+            #sky.setBin("background", 0)
+            #sky.setDepthWrite(False)
+            sky.setScale(6)
+            if not sky.find('**/cloud1').isEmpty() and not sky.find('**/cloud2').isEmpty():
+                sky.find('**/cloud1').setScale(0.6)
+                sky.find('**/cloud2').setScale(0.9)
+                sky.find('**/cloud1').flattenStrong()
+                sky.find('**/cloud2').flattenStrong()
+                self.cloud1_int = sky.find('**/cloud1').hprInterval(360, Vec3(60, 0, 0))
+                self.cloud2_int = sky.find('**/cloud2').hprInterval(360, Vec3(-60, 0, 0))
+            else:
+                sky.clearModelNodes()
+                sky.flattenStrong()
+            self.prepared = True
+    
     def startSky(self, sky):
-        sky.setAttrib(BSPFaceAttrib.makeIgnorePvs(), 1)
-        sky.setLightOff(1)
-        sky.setFogOff(1)
-        #sky.setBin("background", 0)
-        #sky.setDepthWrite(False)
-        sky.setScale(6)
-        if not sky.find('**/cloud1').isEmpty() and not sky.find('**/cloud2').isEmpty():
-            sky.find('**/cloud1').setScale(0.6)
-            sky.find('**/cloud2').setScale(0.9)
-            sky.find('**/cloud1').flattenStrong()
-            sky.find('**/cloud2').flattenStrong()
-            self.cloud1_int = sky.find('**/cloud1').hprInterval(360, Vec3(60, 0, 0))
-            self.cloud2_int = sky.find('**/cloud2').hprInterval(360, Vec3(-60, 0, 0))
+        self.prepareSky(sky)
+        if hasattr(self, 'cloud1_int'):
             self.cloud1_int.loop()
             self.cloud2_int.loop()
-        else:
-            sky.clearModelNodes()
-            sky.flattenStrong()
             
     def stopSky(self):
         if hasattr(self, 'cloud1_int'):
@@ -52,3 +61,4 @@ class SkyUtil:
             del self.cloud1_int
         if hasattr(self, 'cloud2_int'):
             del self.cloud2_int
+        del self.prepared
