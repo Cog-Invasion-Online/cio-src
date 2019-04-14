b1e1d6a532b36095b3dcae456f2ee37b468622b5
Brian Lach <brianlach72@gmail.com>
2019-04-14 19:02:44 -0400

    HDR: don't do auto-exposure if using multithreaded pipeline (until it's fixed)
    Former-commit-id: 53b47638cbd82800218a3807e7f333ae72b4adad

--- game/src/coginvasion/base/HDR.py
+++ game/src/coginvasion/base/HDR.py
@@ -38,7 +38,8 @@ class HDR(DirectObject):
         # We have to refuse HDR on all Intel cards until 
         # they fix their buggy drivers or we find a workaround.
         return (gsg.getSupportsComputeShaders() and
-        		gsg.getDriverVendor() != "Intel")
+        		gsg.getDriverVendor() != "Intel" and
+                not metadata.MULTITHREADED_PIPELINE)
 
     def disable(self):
         base.filters.delExposure()
